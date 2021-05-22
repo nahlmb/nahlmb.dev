7a6412947515bde03e86ad3dda57a26706d64014
Nahl <nahlultra@gmail.com>
2021-05-23 06:58:51 +0700

    hello

---
+++ build/index.js
@@ -0,0 +1,10 @@
+
+injectHeadGlobally()
+
+function injectHeadGlobally() {
+   console.log("inject any things that need globaly");
+}
+
+function toOnProgressPage() {
+   window.location = "./next";
+}
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: {
+    enabled : false,
+    content : ['./build/**/*.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': '"Work Sans", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      'serif': 'ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+      'mono': 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+      'display': ['Montserrat'],
+      'body': ['Work Sans'],
+    },
+    borderRadius: {
+      'none': '0',
+     'sm': '4px',
+     DEFAULT: '8px',
+     'md': '12px',
+     'lg': '24px',
+     'full': '9999px',
+     'large': '24px',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
