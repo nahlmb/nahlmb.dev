2aa33a6bf49d752cad19cf9efd78b893e7a9e896
Nahl Basalamah <nahlultra@gmail.com>
2021-07-03 13:22:59 +0700

    11ty, locoscroll, header fix, and many more

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    enabled : false,
+    enabled : true,
     content : ['./source/**/*.html', './source/**/*.njk']
   },
   darkMode: false, // or 'media' or 'class'
