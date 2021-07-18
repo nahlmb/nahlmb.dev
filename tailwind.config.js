bb9f329f60fb61f5599e34896f64f5d4eb458b14
Nahl Basalamah <nahlultra@gmail.com>
2021-07-18 19:31:57 +0700

    add meta tags

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
   purge: {
-    enabled : false,
+    enabled : true,
     content : ['./source/**/*.html', './source/**/*.njk']
   },
   darkMode: false, // or 'media' or 'class'
